bdffa61306a89834f3594044aafae121b62db698
idatold <idatol01868@stud.noroff.no>
2025-10-06 12:40:29 +0200

    retry build

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./components/Header";
 import SideFooter from "./components/SideFooter";
 import Home from "./routes/Home";
-import Starfield from "./components/Starfield.jsx";
+import Starfield from "./components/Starfield";
 
 export default function App() {
   return (
